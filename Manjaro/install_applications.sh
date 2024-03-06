73ceeb79228f9b8fd1a3698c98d9e1baa27de353
ChampPG <ppgleason02@gmail.com>
2024-03-06 12:21:32 -0500

    Installing applications

--- Manjaro/install_applications.sh
+++ Manjaro/install_applications.sh
@@ -2,3 +2,8 @@
 
 # discord discord-canary telegram syncthing nfs-common obsidian 
 # kali tools (john, hashcat, nmap, burp)
+
+# Standard installed
+sudo pacman -S discord telegram-desktop syncthing \
+  nfs-utils obsidian torbrowser-launcher wireguard-tools nmap \
+  python3 python-virtualenv
