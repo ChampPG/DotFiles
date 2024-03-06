98e788076b9ea66a3fcc7ba27f4ee81445d81a5c
ChampPG <ppgleason02@gmail.com>
2024-03-06 13:01:11 -0500

    Change zshrc

--- Manjaro/install_applications.sh
+++ Manjaro/install_applications.sh
@@ -6,4 +6,15 @@
 # Standard installed
 sudo pacman -S discord telegram-desktop syncthing \
   nfs-utils obsidian torbrowser-launcher wireguard-tools nmap \
-  python3 python-virtualenv
+  python3 python-virtualenv curl wget 
+
+
+cd /home/ppg/Downloads/
+
+wget 'https://discord.com/api/download/canary?platform=linux&format=tar.gz' -O discord-canary.tar.gz
+
+tar -xvf ./discord-canary.tar.gz
+rm ./discord-canary.tar.gz 
+sudo mv ./DiscordCanary /opt/discordcanary
+sudo ln -s /opt/discordcanary/DiscordCanary /usr/bin/
+
