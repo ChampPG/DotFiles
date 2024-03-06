d112df469b18d33bb3d77502f1a9435aa4194963
Paul Gleason <71086240+ChampPG@users.noreply.github.com>
2024-03-06 07:37:27 +0000

    Update install.sh

--- Manjaro/install.sh
+++ Manjaro/install.sh
@@ -12,6 +12,27 @@ USER_HOME=$(eval echo ~$1)
 # Install dependencies
 sudo pacman -S neovim python-pynvim kitty zsh lazygit fzf fd
 
+# Function to move existing files to .bak directory
+move_to_backup() {
+    local file_path="$1"
+    if [ -e "$file_path" ]; then
+        mv "$file_path" "$file_path.bak"
+        echo "Moved $file_path to $file_path.bak"
+    fi
+}
+
+# File directory or file exists move to backup
+move_to_backup "$USER_HOME/powerlevel10k/"
+move_to_backup "$USER_HOME/.config/nvim"
+move_to_backup "$USER_HOME/.config/kitty"
+move_to_backup "$USER_HOME/.config/zsh"
+move_to_backup "$USER_HOME/.config/nvm"
+move_to_backup "$USER_HOME/.fonts/"
+move_to_backup "$USER_HOME/Pictures/Backgrounds"
+move_to_backup "$USER_HOME/.zshrc"
+move_to_backup "$USER_HOME/.zsh_plugins.txt"
+move_to_backup "$USER_HOME/.p10k.zsh"
+
 # Create directories if they don't exist
 mkdir -p "$USER_HOME/powerlevel10k/"
 mkdir -p "$USER_HOME/.config/nvim"
@@ -20,7 +41,6 @@ mkdir -p "$USER_HOME/.config/zsh"
 mkdir -p "$USER_HOME/.config/nvm"
 mkdir -p "$USER_HOME/.fonts/"
 mkdir -p "$USER_HOME/Pictures/Backgrounds"
-
 
 # Symlink files to respective directories
 ln -s "$PWD/Backgrounds" "$USER_HOME/Pictures/Backgrounds"
