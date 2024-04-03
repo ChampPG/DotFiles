7b18e5de0613da0dec136b25efe753b6b9bee5c9
ChampPG <ppgleason02@gmail.com>
2024-04-03 01:05:51 -0400

    NixOS Changes

---
+++ NixOS/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    opts = {
+      filetypes = {
+        markdown = true,
+        help = true,
+        -- add more filetypes here, e.g.
+        python = true,
+        sh = true,
+        yaml = true,
+        lua = true,
+      },
+    },
+  },
+}
